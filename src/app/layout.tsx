e94fb5792ea229f94330d6e180e1a895c52ec3fb
Oscar <oscar1w2e@gmail.com>
2025-05-16 00:55:56 -0600

    add robots and manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,10 +67,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-         {/* Script de GA4 */}
+        {/* Script de GA4 */}
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=G-3WJHMT4J4Z`}
           strategy="afterInteractive"
